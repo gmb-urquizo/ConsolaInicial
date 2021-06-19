d4cc1bf4e7db78f21aafc69f4bbbd0a58237a90d
gmb-urquizo <67298683+gmb-urquizo@users.noreply.github.com>
2021-06-19 18:17:30 -0500

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,34 @@
+﻿namespace MiPrimerConsola
+{
+    class Persona
+    {
+        //encapsulamiento
+        string nombre;
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Apellido { get; set; }
+        public float Nota { get; set; }
+        public int Peso { get; set; }
+        public int Edad { get; set; }
+
+        public string NombreCompleto
+        {
+            get { return $"{this.Nombre} {this.Apellido}"; }//interpolacion
+        }
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+
+        public string Saludar()
+        {
+            return $"hola... ¿cómo estas?, mi nombre es {this.Nombre}";
+        }   
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MiPrimerConsola
+{
+    class Program
+    {
+        static void Main(string[] datosPersona)
+        {
+            Console.WriteLine("Escriba su nombre");
+            var nombre = Console.ReadLine();
+
+            Console.WriteLine("Escriba su apellido");
+            var apellido = Console.ReadLine();
+
+            Console.WriteLine("Escriba su peso");
+            var peso = int.Parse(Console.ReadLine());
+
+            // todo: Rebeca, debo leer 10 datos de una persona utilizando todos esos tipos
+            //float 
+            //double
+            //DateTime
+            //bool
+
+            Console.WriteLine("Escriba su edad");
+            var edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba su nota");
+            var nota = float.Parse(Console.ReadLine());
+
+            var amigo = new Persona
+            {
+                Nombre = nombre,
+                Apellido = apellido,
+                Nota = nota,
+                Peso = peso,
+                Edad = edad
+            };
+
+
+
+            Console.WriteLine($"Hola {amigo.Nombre}");
+            Console.WriteLine($"tu apellido es {amigo.Apellido}");
+            Console.WriteLine("tu nota es  " + amigo.Nota);
+            Console.WriteLine("tu peso es  " + amigo.Peso);
+            Console.WriteLine("tu edad es " + amigo.Edad);
+
+            Persona vecina = new("Cecilia", "Robayo");
+            Console.WriteLine($"tu vecina se llama {vecina.NombreCompleto}");
+            Console.WriteLine("Escriba cualquier cosa para terminar");
+            Console.Read();
+        }
+
+        void Metodo()
+        {
+            /*if (args != null && args.Length > 0)
+           {*/
+        /*Console.WriteLine($"hay{args.Length} argumentos");*/
+        /*Console.WriteLine($"tu nombre es { args[0]}");*/
+
+        /* if (args.Length > 1)
+             Console.WriteLine($"tu apellido  es  {args[1]}");*/
+        /* }
+         else
+             Console.WriteLine("hola desconocido");*/
+    }
+
+    }
+}
